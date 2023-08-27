69cc22b0316dbea2da1d7000c5ae1d9d9de35a5f
Lorik <lorikkmehmeti@gmail.com>
2023-08-27 03:13:07 +0200

    add more padding in daily

--- src/pages/daily/daily.tsx
+++ src/pages/daily/daily.tsx
@@ -66,29 +66,33 @@ export function DailyPage() {
             [1, 2, 3, 4, 5, 6].map(() => {
                return <Skeleton className="w-full h-[32px] mb-3 rounded-md" />;
             })}
-         {!loading &&
-            keywords.length &&
-            keywords.map(({ keyword }, index) => {
-               return (
-                  <button
-                     key={index}
-                     className="w-full text-sm bg-neutral-200/50 backdrop-blur px-2 py-0.5 rounded-md flex items-center transition-colors mb-2"
-                  >
-                     <span className="flex px-3 py-1">
-                        <svg
-                           className={'mr-2.5 h-5 w-5 flex-none fill-slate-400'}
-                           viewBox="0 0 256 256"
-                        >
-                           <path
-                              fill="currentColor"
-                              d="m196.2 132.81l-51.66-19a3.91 3.91 0 0 1-2.32-2.32l-19-51.66a11.93 11.93 0 0 0-22.38 0l-19 51.66a3.91 3.91 0 0 1-2.32 2.32l-51.66 19a11.93 11.93 0 0 0 0 22.38l51.66 19a3.91 3.91 0 0 1 2.32 2.32l19 51.66a11.93 11.93 0 0 0 22.38 0l19-51.66a3.91 3.91 0 0 1 2.32-2.32l51.66-19a11.93 11.93 0 0 0 0-22.38Zm-2.77 14.87l-51.65 19a11.93 11.93 0 0 0-7.07 7.07l-19 51.65a3.92 3.92 0 0 1-7.36 0l-19-51.65a11.93 11.93 0 0 0-7.07-7.07l-51.65-19a3.92 3.92 0 0 1 0-7.36l51.65-19a11.93 11.93 0 0 0 7.07-7.07l19-51.65a3.92 3.92 0 0 1 7.36 0l19 51.65a11.93 11.93 0 0 0 7.07 7.07l51.65 19a3.92 3.92 0 0 1 0 7.36ZM148 40a4 4 0 0 1 4-4h20V16a4 4 0 0 1 8 0v20h20a4 4 0 0 1 0 8h-20v20a4 4 0 0 1-8 0V44h-20a4 4 0 0 1-4-4Zm96 48a4 4 0 0 1-4 4h-12v12a4 4 0 0 1-8 0V92h-12a4 4 0 0 1 0-8h12V72a4 4 0 0 1 8 0v12h12a4 4 0 0 1 4 4Z"
-                           />
-                        </svg>
-                        {keyword.toLowerCase()}
-                     </span>
-                  </button>
-               );
-            })}
+         <div className="pt-2">
+            {!loading &&
+               keywords.length &&
+               keywords.map(({ keyword }, index) => {
+                  return (
+                     <button
+                        key={index}
+                        className="w-full text-sm bg-neutral-200/50 backdrop-blur px-2 py-0.5 rounded-md flex items-center transition-colors mb-2"
+                     >
+                        <span className="flex px-3 py-1">
+                           <svg
+                              className={
+                                 'mr-2.5 h-5 w-5 flex-none fill-slate-400'
+                              }
+                              viewBox="0 0 256 256"
+                           >
+                              <path
+                                 fill="currentColor"
+                                 d="m196.2 132.81l-51.66-19a3.91 3.91 0 0 1-2.32-2.32l-19-51.66a11.93 11.93 0 0 0-22.38 0l-19 51.66a3.91 3.91 0 0 1-2.32 2.32l-51.66 19a11.93 11.93 0 0 0 0 22.38l51.66 19a3.91 3.91 0 0 1 2.32 2.32l19 51.66a11.93 11.93 0 0 0 22.38 0l19-51.66a3.91 3.91 0 0 1 2.32-2.32l51.66-19a11.93 11.93 0 0 0 0-22.38Zm-2.77 14.87l-51.65 19a11.93 11.93 0 0 0-7.07 7.07l-19 51.65a3.92 3.92 0 0 1-7.36 0l-19-51.65a11.93 11.93 0 0 0-7.07-7.07l-51.65-19a3.92 3.92 0 0 1 0-7.36l51.65-19a11.93 11.93 0 0 0 7.07-7.07l19-51.65a3.92 3.92 0 0 1 7.36 0l19 51.65a11.93 11.93 0 0 0 7.07 7.07l51.65 19a3.92 3.92 0 0 1 0 7.36ZM148 40a4 4 0 0 1 4-4h20V16a4 4 0 0 1 8 0v20h20a4 4 0 0 1 0 8h-20v20a4 4 0 0 1-8 0V44h-20a4 4 0 0 1-4-4Zm96 48a4 4 0 0 1-4 4h-12v12a4 4 0 0 1-8 0V92h-12a4 4 0 0 1 0-8h12V72a4 4 0 0 1 8 0v12h12a4 4 0 0 1 4 4Z"
+                              />
+                           </svg>
+                           {keyword.toLowerCase()}
+                        </span>
+                     </button>
+                  );
+               })}
+         </div>
       </React.Fragment>
    );
 }
